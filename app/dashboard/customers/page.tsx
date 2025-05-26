8a74d9591a824d2b9491fd1245ff8d95c66bb7d7
minseo-hong <mshong1007@gmail.com>
2025-05-27 07:48:25 +0900

    Complete Next.js tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return <Table customers={customers} />;
 }
